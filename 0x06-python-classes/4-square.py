8f8aa45ed726e7bbbee1ba6395af3e286962ad9d
Olusegun Ajayi <olusegunajayi09@gmail.com>
2022-10-11 14:05:02 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Square based on private instance attribute"""
+
+
+class Square:
+    """Represents a new square."""
+    def __init__(self, size=0):
+        """Initialise a new square.
+        args:
+            size(int): size of the new aquare.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get/set the value for the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return (self.__size * self.__size)
